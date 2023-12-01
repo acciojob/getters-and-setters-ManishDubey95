daebea72bfd678b49d70e740301098a77bdbbc4a
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-12-01 17:08:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -41,6 +41,14 @@ class Teacher extends Person {
   }
 }
 
+const student = new Student("John", 20);
+student.study(); // Output: John is studying
+console.log(`Student's age: ${student.age}`); // Output: Student's age: 20
+
+const teacher = new Teacher("Professor Smith", 35);
+teacher.teach(); // Output: Professor Smith is teaching
+console.log(`Teacher's age: ${teacher.age}`); // Output: Teacher's age: 35
+
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
